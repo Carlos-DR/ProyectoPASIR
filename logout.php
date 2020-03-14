a2e9180162699e707e5a4f7ae5baf966749cfbb0
Carlos-DR <60520677+Carlos-DR@users.noreply.github.com>
2020-03-14 11:18:18 +0100

    Empezar de 0

--- logout.php
+++
@@ -1,8 +0,0 @@
-<!-- Con esto cerramos sesión y redirige al index -->
-<?php
-    session_start();
-    unset($_SESSION["user"]); 
-    session_destroy();
-    header("Location: index.php");
-    exit;
-?>
